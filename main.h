9233b966d0d382d8e787117ca34cb8e7eaa68819
macahmed1 <malikprog@yahoo.com>
2023-09-17 22:47:49 -0700

    Header file with the prototype of the _printf function:

--- main.h
+++ main.h
@@ -4,13 +4,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-typedef struct Cspecs {
-        char cs;
-        void (*f)();
-} cs_t;
-
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
-
 
