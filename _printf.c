b1c9964e79cf9308aab6ac662cd0384bc4f1e159
macahmed1 <malikprog@yahoo.com>
2023-09-18 00:59:49 -0700

    A function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,59 +1,55 @@
-#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-
+#include "main.h"
 /**
- * _printf - Custom printf function
+ * _printf - Custom printf function.
  * @format: The format string containing conversion specifiers
  * @...: The values to be printed based on the format
- *
- * Return: The number of characters printed (excluding the null byte)
+ * Return: Returns the number of characters print (exclude bytes)
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
-	int i = 0;
+	int count = 0, i = 0;
 
 	va_start(args, format);
 	while (format && format[i])
 	{
-		if (format[i] != '%')
-		{
-			putchar(format[i]);
-			count++;
-		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			putchar('%');
-			count++;
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			char c = va_arg(args, int);
+	if (format[i] != '%')
+	{
+		putchar(format[i]);
+		count++;
+	}
+	else if (format[i] == '%' && format[i + 1] == '%')
+	{
+		putchar('%');
+		count++;
+		i++;
+	}
+	else if (format[i] == '%' && format[i + 1] == 'c')
+	{
+		char c = va_arg(args, int);
 
-			putchar(c);
-			count++;
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 's')
-		{
+		putchar(c);
+		count++;
+		i++;
+	}
+	else if (format[i] == '%' && format[i + 1] == 's')
+	{
 		char *str = va_arg(args, char *);
 
-			if (str)
-			{
-				while (*str)
-				{
-					putchar(*str);
-					count++;
-					str++;
-				}
-			}
-			i++;
+		if (str)
+		{
+		while (*str)
+		{
+		putchar(*str);
+		count++;
+		str++;
 		}
+	}
 		i++;
+	}
+	i++;
 	}
 	va_end(args);
 	return (count);
